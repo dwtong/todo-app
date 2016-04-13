f379b8a07e5dbfdc0eec45da5bea03d7a9170bf4
Dan Tong <danieltongnz@gmail.com>
2016-04-13 20:45:11 +1200

    Refactor controller - db is no longer a parameter, add function as exports methods

--- controller.js
+++ controller.js
@@ -1,90 +1,81 @@
-module.exports = function (Db) {
+var Todo = require('./app/models/todo');
 
-  var deleteTodo = function (req, res) {
-    Db.remove({
-        _id: req.params.todo_id
-      },
-      function (err, todo) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.json({
-            message: 'Successfully deleted.'
-          });
-        }
-
-      });
-  };
-
-  var getTodo = function (req, res) {
-
-    // Find todo item in database based on item id
-    Db.findById(req.params.todo_id, function (err, todo) {
+exports.deleteTodo = function (req, res) {
+  Todo.remove({
+      _id: req.params.todo_id
+    },
+    function (err, todo) {
       if (err) {
         res.send(err);
-
       } else {
-        res.json(todo);
-      }
-    });
-  }
-
-  var getAllTodos = function (req, res) {
-    Db.find(function (err, todo) {
-      if (err) {
-        res.send(err);
-      }
-
-      res.json(todo);
-    });
-  };
-
-  var putTodo = function (req, res) {
-    Db.findById(req.params.todo_id, function (err, todo) {
-      if (err) {
-        res.send(err);
-      }
-
-      // Update todo description to the one in the request
-      todo.description = req.body.description;
-
-      // Save updated todo in Db
-      todo.save(function (err) {
-        if (err) {
-          res.send(err);
-        }
-
         res.json({
-          message: 'Todo updated.'
+          message: 'Successfully deleted.'
         });
-      });
+      }
+
     });
-  };
+};
 
-  var postTodo = function (req, res) {
+exports.getTodo = function (req, res) {
 
-    // Create new instance of Todo Model
-    var todo = new Db();
+  // Find todo item in database based on item id
+  Todo.findById(req.params.todo_id, function (err, todo) {
+    if (err) {
+      res.send(err);
 
-    // Set the todo description to the description in the request
+    } else {
+      res.json(todo);
+    }
+  });
+}
+
+exports.getAllTodos = function (req, res) {
+  Todo.find(function (err, todo) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(todo);
+  });
+};
+
+exports.putTodo = function (req, res) {
+  Todo.findById(req.params.todo_id, function (err, todo) {
+    if (err) {
+      res.send(err);
+    }
+
+    // Update todo description to the one in the request
     todo.description = req.body.description;
 
-    // Save todo item in DB
+    // Save updated todo in Todo
     todo.save(function (err) {
       if (err) {
         res.send(err);
       }
 
-      // Response contains todo object
-      res.json(todo);
+      res.json({
+        message: 'Todo updated.'
+      });
     });
-  };
+  });
+};
 
-  return {
-    deleteTodo: deleteTodo,
-    getTodo: getTodo,
-    getAllTodos: getAllTodos,
-    postTodo: postTodo,
-    putTodo: putTodo
-  }
+exports.postTodo = function (req, res) {
+
+  // Create new instance of Todo Model
+  var todo = new Todo();
+
+  // Set the todo description to the description in the request
+  todo.description = req.body.description;
+
+  // Save todo item in DB
+  todo.save(function (err) {
+    if (err) {
+      res.send(err);
+    }
+
+    // Response contains todo object
+    res.json(todo);
+  });
 };
